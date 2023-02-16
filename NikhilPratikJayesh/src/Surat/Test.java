945e6e5233d1a0951c8e8e18438cda46daaa8d61
ADMIN <ADMIN@DESKTOP-U2QV9TM>
2023-02-16 18:41:07 +0530

    First Commit

---
+++ NikhilPratikJayesh/src/Surat/Test.java
@@ -0,0 +1,23 @@
+package Surat;
+
+public class Test {
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int year = 2016;
+      
+      if(year % 400==0 || (year % 4==0 && year % 100!= 0))
+      {
+    	  System.out.println("leap year");
+      }
+      else
+      {
+    	  System.out.println("Not leap year");
+      }
+	}
+
+}
+
+
+
